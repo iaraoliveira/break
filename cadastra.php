a7c8e410e241d0c50b870aa10763deea0e0a006a
Iara Silva de Oliveira <iara99oliveira@gmail.com>
2017-11-13 20:54:32 -0200

    adicionado os campos necessários do cadastra.php    PS.: Sim, está tudo explodindo, eu sei

--- cadastra.php
+++ cadastra.php
@@ -27,11 +27,11 @@
 	</script>
 </head>
 <body onload="loading()">
-		
+
 	<div class="box-that">
 		<img id="hominho" src="img/jedi_hominho.png"
 			 alt="">
-		
+
 		<div id="sub-box">
 			<h1><span id="h1_minus">COME TO THE </span>
 									BREAK SIDE
@@ -40,22 +40,53 @@
 			</h1>
 			<form name="cadastro" action="verificaDados.php" method="post" onsubmit="">
 				<label for="f_name_input">Primeiro Nome</label>
-				<input type="text" id="f_name_input" title="Primeiro nome" placeholder="ex.: Luke" required autofocus name="f_name_input"/> 
+				<input type="text" id="f_name_input" title="Primeiro nome" placeholder="ex.: Luke" required autofocus name="f_name_input"/>
 				<br/>
-				
+
 				<label for="s_name_input">Sobrenome</label>
-				<input type="text" id="s_name_input" title="Sobrenome" placeholder="ex.: Skybreaker" required name="s_name_input"/> 
+				<input type="text" id="s_name_input" title="Sobrenome" placeholder="ex.: Skybreaker" required name="s_name_input"/>
 				<br/>
-			
-				
+
+				<label for="email_input">Email</label>
+				<input type="email" id="email_input" title="Email" placeholder="ex.: lukeskybreaker@break.com" required name="email_input"/>
+				<br/>
+
+				<label for="login_input">Login</label>
+				<input type="text" id="login_input" title="Login" placeholder="ex.: JediMaster123" required name="login_input"/>
+				<br/>
+
+				<label for="senha_input">Senha</label>
+				<input type="password" id="senha_input" title="Senha" placeholder="Digite sua power senha aqui" required name="senha_input"/>
+				<br/>
+
+				<div id=radio-genero>
+					<label for="genero_input">Gênero</label>
+					<input type="radio" id="genero_m_input" name="genero_input" value="masc"> Masculino
+	  			<input type="radio" id="genero_f_input"name="genero_input" value="fem"> Feminino
+				</div>
+				<br/>
+
+				<label for="datanasc_input">Data de Nascimento</label>
+				<input type="date" id="datanasc_input" title="Data de Nascimento" required name="datanasc_input"/>
+				<br/>
+
+				<label for="escolaridade_input">Escolaridade</label>
+				<select id="escolaridade_input" name="escolaridade_input">
+				  <option value="fundamental1">Ensino Fundamental I</option>
+				  <option value="fundamental2">Ensino Fundamental II</option>
+				  <option value="medio">Ensino Médio</option>
+				  <option value="superior">Ensino Superior</option>
+				</select>
+
+
 				<div id="down-inputs">
 					<a href="login.php" title="Entrar">Já sou um jedi.</a>
 					<input type="submit" id="button-entry" title="wanna be a jedi, now." value="CADASTRAR"/>
 				</div>
 			</form>
-			
-			
-		</div> 
+
+
+		</div>
 		<div id="rodape">
 			<a href="promocional.php" title="Ma que cartso que é breakuro?" id="saiba_mais">O que é break?</a>
 		</div>
