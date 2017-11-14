36bc73e6eb3d081a08d6792c2d0f151cc74e3813
Iara Silva de Oliveira <iara99oliveira@gmail.com>
2017-11-13 22:59:53 -0200

    Modificações no verificaDados.php...    Thanks, Kira.. but i don't know PHP ;-;

--- verificaDados.php
+++ verificaDados.php
@@ -1,6 +1,38 @@
 <?
 /* verificaDados.php */
+session_start();
+/* arquivo de configuração */
+require 'config-painel.php';
+/* Conexão com BD */
+require 'classes/Db.class.php';
 
+if($_POST){
+  /* iniciar o cadastro do usuário */
+  $banco->bind('nome',$_POST['nome_user']);
+  $banco->bind('sobrenome',$_POST['sobrenome_user']);
+  $banco->bind('email',$_POST['email_user']);
+  $banco->bind('senha',$_POST['senha']);
+  $banco->bind('genero',$_POST['sexo']);
+  $banco->bind('dataNasc',$_POST['dataNasc']);
+  $banco->bind('escolaridade',$_POST['escolaridade']);
+
+$query = "INSERT INTO usuario (nome_user, sobrenome_user, email, login, senha, sexo, dataNasc, escolaridade)
+                    VALUES (:nome, :sobrenome, :email, :senha, :genero, :dataNasc, :escolaridade)";
+
+$insercao = $banco->query($query);
+
+/* mensagem de sucesso/erro de cadastro */
+
+	$_SESSION['cadastro'] = $insercao;
+	if($insercao){
+		header("Location: index.php");
+	}else{
+		echo "<script>alert('A unknown bug has appeared.. :c');</script>"
+	}
+} /* Fecha if POST */
+
+/*
+$banco = new DB();
 $nome=$_POST['nome_user'];
 $sobrenome=$_POST['sobrenome_user'];
 $email=$_POST['email'];
@@ -11,4 +43,6 @@ $escolaridade=$_POST['escolaridade'];
 
 $sql = mysql_query ("INSERT INTO usuario (nome_user, sobrenome_user, email, login, senha, sexo, dataNasc, escolaridade)
                     VALUES ('$nome', '$sobrenome', '$email', '$senha', '$genero', '$dataNasc', '$escolaridade')");
+
+*/
 ?>
